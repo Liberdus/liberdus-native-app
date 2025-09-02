306546034f986c93e47bcbb9180f275674aed984
jairajdev <jairajranamagar.dev@gmail.com>
2025-09-02 21:08:31 +0545

    Replace in-memory message deduplication with AsyncStorage persistence

--- app.config.js
+++ app.config.js
@@ -2,10 +2,11 @@ import config from './app.json';
 
 export default ({ config: _ }) => {
   const plugins = [...config.expo.plugins];
-  
-  // Only add Firebase plugin for Android builds
+
+  // Only add Firebase plugins for Android builds
   if (process.env.EAS_BUILD_PLATFORM === 'android') {
     plugins.push('@react-native-firebase/app');
+    plugins.push('@react-native-firebase/messaging');
   }
 
   return {
